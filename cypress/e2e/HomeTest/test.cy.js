166b0ab0f3a1c2f235cb132250a12bda5d6cf7a4
Arshad-SecureTech <arshad.mehmood@securetech-consultancy.com>
2023-03-29 01:59:07 +0500

    Code Merge: Changes 1. Code added

--- cypress/e2e/HomeTest/test.cy.js
+++ cypress/e2e/HomeTest/test.cy.js
@@ -2,7 +2,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("I visit duckduckgo.com", () => {
-  cy.visit("https://www.duckduckgo.com");
+  cy.visit("https://www.google.com);
 });
 
 When("I do something", ()=>{
@@ -11,9 +11,5 @@ When("I do something", ()=>{
 })
 
 Then("I should see a search bar", () => {
-  cy.get("input").should(
-    "have.attr",
-    "placeholder",
-    "Search the web without being tracked"
-  );
+
 });
