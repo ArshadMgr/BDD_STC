396f9c39fb8e52c009c3fea0a16cd5384b00bd68
Arshad-SecureTech <arshad.mehmood@securetech-consultancy.com>
2023-04-16 11:19:12 +0500

    URL updated

--- cypress/e2e/HomeTest/test.cy.js
+++ cypress/e2e/HomeTest/test.cy.js
@@ -2,7 +2,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("I visit duckduckgo.com", () => {
-  cy.visit("https://www.google.com")
+  cy.visit("https://www.youtube.com")
 });
 
 When("I do something", ()=>{
